5c0d08956ebc126dd6443f367d24c141d72de061
YoginiKottewar <114603773+YoginiKottewar@users.noreply.github.com>
2022-12-22 21:26:02 +0530

    Update home-page.component.ts

--- grocessary-management-front-end/src/app/components/home-page/home-page.component.ts
+++ grocessary-management-front-end/src/app/components/home-page/home-page.component.ts
@@ -1,4 +1,6 @@
+import { Router, RouterModule } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import {GrocessaryService} from '../service/grocessary.service';
 
 @Component({
   selector: 'app-home-page',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router,
+    private gservice:GrocessaryService) { }
 
   ngOnInit(): void {
+  }
+  viewall():void{
+    console.log("test");
+
+if (this.gservice.getClientAuthorization() !== null){
+  this.router.navigate(["/client/home"])
+
+}else{
+  this.router.navigate(["/client-login"])
+
+}
+
   }
 
 }
